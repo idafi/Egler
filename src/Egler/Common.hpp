e110a17769d93b654886bdcaea3b11aa58c8faaf
idafi <none>
2018-07-26 15:35:39 -0500

    port over math helpers

--- src/Egler/Common.hpp
+++ src/Egler/Common.hpp
@@ -13,6 +13,11 @@
 
 #define M_PI 3.14159265358979323846
 
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define Clamp(value, lower, upper) (min(max((value), (lower)), (upper)))
+#define randf() ((float)(rand()) / (float)(RAND_MAX))
+
 #define FlagSet(mask, flag) ((mask) |= (flag))
 #define FlagClear(mask, flag) ((mask) &= ~(flag))
 #define FlagTest(mask, flag) (((mask) & (flag)) == (flag))
