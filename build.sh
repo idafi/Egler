70b2b92ec7a04c2adb616f2a727ffff9f58140ff
idafi <none>
2018-07-26 23:29:43 -0500

    only build tests when running tests

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-clang++ \
-    -target x86_64-pc-windows-gnu \
-    -g \
-    -O0 \
-    -std=c++17 \
-    -Wall \
-    -I"/mingw64/include/SDL2/" \
-    -o"bin/Egler.exe" \
-    "src/Egler/Egler.cpp" \
-    -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2
+shopt -s globstar
 
 clang++ \
     -target x86_64-pc-windows-gnu \
@@ -16,6 +7,6 @@ clang++ \
     -std=c++17 \
     -Wall \
     -I"/mingw64/include/SDL2/" \
-    -o"bin/Egler.Test.exe" \
-    `ls src/Egler.Test/*.cpp` \
-    -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2 -lgtest_main -lgtest
+    -o"bin/Egler.exe" \
+    `ls src/Egler/**/*.cpp` \
+    -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2
--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 echo "Building..."
-./build.sh
+
+clang++ \
+    -target x86_64-pc-windows-gnu \
+    -g \
+    -O0 \
+    -std=c++17 \
+    -Wall \
+    -I"/mingw64/include/SDL2/" \
+    -o"bin/Egler.Test.exe" \
+    `ls src/Egler.Test/**/*.cpp` \
+    -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2 -lgtest_main -lgtest
 
 if [ $? -eq 0 ]; then
     echo "Done."
