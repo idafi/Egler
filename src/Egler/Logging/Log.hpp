1d389f5c121e0b4077a063aaa2c3b5f04e273dfa
idafi <none>
2018-07-27 09:50:03 -0500

    hide default log instnace behind static methods

--- src/Egler/Logging/Log.hpp
+++ src/Egler/Logging/Log.hpp
@@ -9,23 +9,27 @@
 class Log
 {
 	public:
-		static Log& Default();
+		static void AddDefaultLogger(ILogger * const logger, const LogLevel minLevel);
+		static void RemoveDefaultLogger(ILogger * const logger);
+		static void ChangeDefaultMinLevel(ILogger * const logger, const LogLevel minLevel);
+		static void WriteToDefault(const LogLevel level, char const * const msg, ...);
 	
 		void AddLogger(ILogger * const logger, const LogLevel minLevel);
 		void RemoveLogger(ILogger * const logger);
 		void ChangeMinLevel(ILogger * const logger, const LogLevel minLevel);
 		
-		void Write(LogLevel level, char const * const msg, ...);
+		void Write(const LogLevel level, char const * const msg, ...);
 		
 	private:
 		static Log defaultLog;
 		std::map<ILogger * const, LogLevel> loggers;
 		
 		std::string GetTimestamp();
+		void Write(const LogLevel level, char const * const msg, va_list args);
 };
 
-#define LogDebug(msg, ...) Log::Default().Write(LogLevel::Debug, msg "\n", ##__VA_ARGS__)
-#define LogNote(msg, ...) Log::Default().Write(LogLevel::Note, msg "\n", ##__VA_ARGS__)
-#define LogWarning(msg, ...) Log::Default().Write(LogLevel::Warning, msg "\n", ##__VA_ARGS__)
-#define LogError(msg, ...) Log::Default().Write(LogLevel::Error, msg "\n", ##__VA_ARGS__)
-#define LogFailure(msg, ...) Log::Default().Write(LogLevel::Failure, msg "\n", ##__VA_ARGS__)
+#define LogDebug(msg, ...) Log::WriteToDefault(LogLevel::Debug, msg "\n", ##__VA_ARGS__)
+#define LogNote(msg, ...) Log::WriteToDefault(LogLevel::Note, msg "\n", ##__VA_ARGS__)
+#define LogWarning(msg, ...) Log::WriteToDefault(LogLevel::Warning, msg "\n", ##__VA_ARGS__)
+#define LogError(msg, ...) Log::WriteToDefault(LogLevel::Error, msg "\n", ##__VA_ARGS__)
+#define LogFailure(msg, ...) Log::WriteToDefault(LogLevel::Failure, msg "\n", ##__VA_ARGS__)
