de55d152758d139ed0297fd5bdf9834b4db692f7
idafi <none>
2018-07-27 11:40:11 -0500

    use window object in test entry point

--- src/Egler/Egler.cpp
+++ src/Egler/Egler.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
-#include "../Egler.Core/Common.hpp"
+#include "../Egler.Core/Core.hpp"
+#include "../Egler.Core/Video/Video.hpp"
 
 int main(int argc, char **argv)
 {
@@ -8,17 +9,21 @@ int main(int argc, char **argv)
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("Egler",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480,
-        SDL_RENDERER_PRESENTVSYNC
-    );
+    PixelRect rect(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480);
+    Window *window = new Window("Egler", rect);
 
     LogNote("hello !!");
 
     while(!SDL_QuitRequested())
-    { SDL_Delay(16); }
+    {
+        Vector4 color(0, 1, 1, 1);
+        window->Clear(color, 1);
+        window->Present();
+        
+        SDL_Delay(16);
+    }
 
-    SDL_DestroyWindow(window);
+    delete window;
     SDL_Quit();
 
     Log::RemoveDefaultLogger(logger);
