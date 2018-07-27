e70a695629e08958e80c076f2b7f355bd8c23d26
idafi <none>
2018-07-27 16:28:22 -0500

    add new includes and types to video system header

--- src/Egler.Core/Video/Video.hpp
+++ src/Egler.Core/Video/Video.hpp
@@ -7,5 +7,13 @@
 #include <GL/glew.h>
 #include "../Core.hpp"
 
+typedef GLuint vertex_array;
+typedef GLuint vertex_buffer;
+typedef GLuint index_buffer;
+
 #include "PixelRect.hpp"
-#include "Window.hpp"
+#include "ModelBuffer.hpp"
+#include "Model.hpp"
+#include "ModelPool.hpp"
+#include "GLWindow.hpp"
+#include "GLContext.hpp"
