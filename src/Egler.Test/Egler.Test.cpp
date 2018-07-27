d0146e5771ea0df8939a649a078440ebc8bc9b3f
idafi <none>
2018-07-27 10:02:31 -0500

    add gmock headers and init to test lib

--- src/Egler.Test/Egler.Test.cpp
+++ src/Egler.Test/Egler.Test.cpp
@@ -4,6 +4,6 @@ int main(int argc, char **argv)
 {
     _set_error_mode(_OUT_TO_STDERR);
 
-    testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- src/Egler.Test/Egler.Test.hpp
+++ src/Egler.Test/Egler.Test.hpp
@@ -1,2 +1,5 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
 #include "../Egler/Common.hpp"
+#include "../Egler/Logging/Logging.hpp"
