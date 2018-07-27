1aab133c6267962e597e8e60ca7c4744a6602120
idafi <none>
2018-07-27 14:12:17 -0500

    implement and test basic allocator pool

---
+++ src/Egler.Core/Pool.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <queue>
+#include "Core.hpp"
+
+template<typename T, int size>
+class Pool
+{
+    public:
+        struct Ptr
+        {
+            int Index, Revision;
+
+            Ptr() : Index(-1), Revision(0) { }
+            Ptr(int index, int revision) : Index(index), Revision(revision) { }
+        };
+
+        Pool()
+        {
+            for(int i = 0; i < size; i++)
+            { freeIndices.push(i); }
+
+            memset(values, 0, sizeof(values));
+        }
+
+        T& operator [](const Ptr& ptr)
+        {
+            assert(IsAllocated(ptr));
+
+            return values[ptr.Index].Item;
+        }
+
+        int Capacity()
+        {
+            return size;
+        }
+
+        int Count()
+        {
+            return (size - freeIndices.size());
+        }
+
+        Ptr Allocate()
+        {
+            assert(freeIndices.size() > 0);
+            
+            int i = freeIndices.front();
+            freeIndices.pop();
+            values[i].IsAllocated = true;
+
+            Ptr ptr(i, values[i].Revision);
+            return ptr;
+        }
+
+        void Free(const Ptr& ptr)
+        {
+            if(IsAllocated(ptr))
+            {
+                Value *val = &values[ptr.Index];
+                val->IsAllocated = false;
+                val->Revision++;
+
+                freeIndices.push(ptr.Index);
+            }
+        }
+
+        bool IsAllocated(const Ptr& ptr)
+        {
+            if(ptr.Index > -1 && ptr.Index < size)
+            {
+                if(values[ptr.Index].IsAllocated)
+                { return (ptr.Revision == values[ptr.Index].Revision); }
+            }
+
+            return false;
+        }
+
+    private:
+        struct Value
+        {
+            T Item;
+
+            bool IsAllocated;
+            int Revision;
+        };
+
+        Value values[size];
+        std::queue<int> freeIndices;
+};
---
+++ src/Egler.Test/PoolTest.cpp
@@ -0,0 +1,62 @@
+#include "Egler.Test.hpp"
+
+class PoolTest : public testing::Test
+{
+    protected:
+        Pool<int, 5> pool;
+};
+
+TEST_F(PoolTest, TestAllocate)
+{
+    auto ptr = pool.Allocate();
+    pool[ptr] = 666;
+
+    ASSERT_EQ(1, pool.Count());
+    ASSERT_EQ(666, pool[ptr]);
+}
+
+TEST_F(PoolTest, TestAllocateFailsWhenFull)
+{
+    for(int i = 0; i < pool.Capacity(); i++)
+    { pool.Allocate(); }
+
+    ASSERT_DEATH(pool.Allocate(), ".*");
+}
+
+TEST_F(PoolTest, TestReallocateFreed)
+{
+    Pool<int, 5>::Ptr ptrs[5];
+    for(int i = 0; i < 5; i++)
+    { ptrs[i] = pool.Allocate(); }
+    for(int i = 0; i < 5; i++)
+    { pool.Free(ptrs[i]); }
+
+    for(int i = 0; i < 5; i++)
+    { ASSERT_NO_FATAL_FAILURE(pool.Allocate()); }
+}
+
+TEST_F(PoolTest, TestFree)
+{
+    auto ptr = pool.Allocate();
+    pool[ptr] = 666;
+    pool.Free(ptr);
+
+    ASSERT_EQ(0, pool.Count());
+    ASSERT_DEATH(pool[ptr], ".*");
+}
+
+TEST_F(PoolTest, TestFreeHandlesNotAllocated)
+{
+    auto ptr = pool.Allocate();
+    pool.Free(ptr);
+    ASSERT_NO_FATAL_FAILURE(pool.Free(ptr));
+}
+
+TEST_F(PoolTest, TestIsAllocated)
+{
+    auto ptr = pool.Allocate();
+    ASSERT_TRUE(pool.IsAllocated(ptr));
+
+    pool.Free(ptr);
+    ASSERT_FALSE(pool.IsAllocated(ptr));
+}
