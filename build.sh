ad12fcfed25a33c72ee7326ea6be3d337e674b8c
idafi <none>
2018-07-27 10:34:08 -0500

    compile main code as shared lib

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 shopt -s globstar
 
 clang++ \
-    -mwindows \
+    -shared \
     -target x86_64-pc-windows-gnu \
     -g \
     -O0 \
     -std=c++17 \
     -Wall \
     -I"/mingw64/include/SDL2/" \
-    -o"bin/Egler.exe" \
+    -o"bin/Egler.dll" \
     `ls src/Egler/**/*.cpp` \
     -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2
