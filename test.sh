31c5278f64afb32b829602361e10df0cc589dbc6
idafi <none>
2018-07-27 10:34:45 -0500

    test build just links to main shared lib

--- test.sh
+++ test.sh
@@ -8,11 +8,9 @@ clang++ \
     -O0 \
     -std=c++17 \
     -Wall \
-    -I"/mingw64/include/SDL2/" \
     -o"bin/Egler.Test.exe" \
-    `ls src/Egler/**/*.cpp` \
     `ls src/Egler.Test/**/*.cpp` \
-    -lOpenGL32 -lglew32 -lmingw32 -lSDL2main -lSDL2 \
+    -L"bin" -lEgler \
     -lgtest_main -lgmock_main -lgtest -lgmock
 
 if [ $? -eq 0 ]; then
