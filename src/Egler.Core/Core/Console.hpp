d16bcd311dab4eca4499801da99775dac9f8359a
idafi <none>
2018-07-28 09:51:19 -0500

    move core system files to core folder

---
+++ src/Egler.Core/Core/Console.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Core.hpp"
+
+// - - - - - -
+// platform-independent console interface
+// - - - - - -
+
+enum class ConsoleColor
+{
+	Black,
+	Grey,
+	White,
+	Red,
+	Yellow
+};
+
+class Console
+{
+	public:
+		Console();
+		~Console();
+		
+		void SetColor(ConsoleColor foreground, ConsoleColor background);
+		void Print(const char * const msg);
+		
+	private:
+		FILE *dummy;
+		
+		ushort GetForegroundColor(ConsoleColor color);
+		ushort GetBackgroundColor(ConsoleColor color);
+};
