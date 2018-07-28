6f6955668a09f1f8d8c29ddf22784eceff305ec3
idafi <none>
2018-07-27 23:37:33 -0500

    add remaining check tests

--- src/Egler.Test/CheckTest.cpp
+++ src/Egler.Test/CheckTest.cpp
@@ -18,6 +18,21 @@ TEST_F(CheckTest, TestCheckPtr)
     ASSERT_NO_THROW(CheckPtr(p));
 }
 
+TEST_F(CheckTest, TestCheckID)
+{
+    ASSERT_THROW(CheckID(0), BadArgumentException);
+    ASSERT_THROW(CheckID(-1), BadArgumentException);
+    ASSERT_NO_THROW(CheckID(1));
+}
+
+TEST_F(CheckTest, TestCheckSign)
+{
+    ASSERT_THROW(CheckSign(-1), OutOfRangeException);
+    ASSERT_THROW(CheckSign(-0.00001f), OutOfRangeException);
+    ASSERT_NO_THROW(CheckSign(1));
+    ASSERT_NO_THROW(CheckSign(0));
+}
+
 TEST_F(CheckTest, TestCheckIndex)
 {
     ASSERT_THROW(CheckIndex(5, 5), OutOfRangeException);
@@ -27,10 +42,10 @@ TEST_F(CheckTest, TestCheckIndex)
     ASSERT_NO_THROW(CheckIndex(4, 5));
 }
 
-TEST_F(CheckTest, TestCheckSign)
+TEST_F(CheckTest, TestCheckRange)
 {
-    ASSERT_THROW(CheckSign(-1), OutOfRangeException);
-    ASSERT_THROW(CheckSign(-0.00001f), OutOfRangeException);
-    ASSERT_NO_THROW(CheckSign(1));
-    ASSERT_NO_THROW(CheckSign(0));
+    ASSERT_THROW(CheckRange(6, 5), OutOfRangeException);
+    ASSERT_THROW(CheckRange(-1, 5), OutOfRangeException);
+    ASSERT_NO_THROW(CheckRange(0, 5));
+    ASSERT_NO_THROW(CheckRange(5, 5));
 }
