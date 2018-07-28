b42a20cabe6ae479ece839c03a62d551abe2edac
idafi <none>
2018-07-28 10:39:39 -0500

    add logging for models and model pool

--- src/Egler.Core/Video/Model.cpp
+++ src/Egler.Core/Video/Model.cpp
@@ -1,5 +1,7 @@
 #include "Video.hpp"
 
+using namespace Egler::Logging;
+
 namespace Egler::Video
 {
     Model::Model()
@@ -12,31 +14,42 @@ namespace Egler::Video
 
     void Model::SetData(const ModelBuffer& buffer)
     {
+        LogDebug("Setting model data...");
+
         CheckID(vao);
         CheckID(vbo_pos);
         CheckID(vbo_col);
         CheckID(ibo);
 
+        LogDebug("...binding vertex array...");
         glBindVertexArray(vao);
 
-        // add position data
+        LogDebug("...pushing vertex positions...");
+        
         glBindBuffer(GL_ARRAY_BUFFER, vbo_pos);
         glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * buffer.VertexCount, buffer.VertexPositions, GL_STATIC_DRAW);
         glEnableVertexAttribArray((int)(VertexAttr::Position));
         glVertexAttribPointer((int)(VertexAttr::Position), 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        LogDebug("...pushing vertex colors...");
         
         // add color data
         glBindBuffer(GL_ARRAY_BUFFER, vbo_col);
         glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 4 * buffer.VertexCount, buffer.VertexColors, GL_STATIC_DRAW);
         glEnableVertexAttribArray((int)(VertexAttr::Color));
         glVertexAttribPointer((int)(VertexAttr::Color), 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+        LogDebug("...pushing indices...");
         
         // add data indices
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ushort) * buffer.IndexCount, buffer.Indices, GL_STATIC_DRAW);
 
+        LogDebug("...unbinding vertex array...");
         glBindVertexArray(0);
 
         indexCount = buffer.IndexCount;
+
+        LogDebug("...done");
     }
 }
--- src/Egler.Core/Video/ModelPool.cpp
+++ src/Egler.Core/Video/ModelPool.cpp
@@ -1,16 +1,24 @@
 #include "Video.hpp"
 
+using namespace Egler::Logging;
+
 namespace Egler::Video
 {
 	ModelPool::ModelPool()
 	{
+		LogNote("Creating model pool...");
+
 		if(!SDL_GL_GetCurrentContext())
 		{ throw NotInitializedException("No GL context has been initialized yet."); }
+
+		LogDebug("...generating GL buffers...");
 
 		glGenBuffers(maxVAOs, vaos);
 		glGenBuffers(maxVBOs, vbos);
 		glGenBuffers(maxIBOs, ibos);
 		
+		LogDebug("...assigning buffer IDs to models...");
+
 		std::unique_ptr<Model[]> initModels(new Model[MaxModels]);
 		for(int i = 0; i < MaxModels; i++)
 		{
@@ -22,15 +30,24 @@ namespace Egler::Video
 			initModels[i] = Model(vao, vbo_pos, vbo_col, ibo);
 		}
 
+		LogDebug("...creating pool...");
 		models = Pool<Model, MaxModels>(initModels.get(), MaxModels);	
+
+		LogNote("...done.");
 	}
 
 	ModelPool::~ModelPool()
 	{
+		LogNote("Destroying model pool...");
+
+		LogDebug("...clearing pool...");
+		models.Clear();
+
+		LogDebug("...deleting GL buffers...");
 		glDeleteBuffers(maxIBOs, ibos);
 		glDeleteBuffers(maxVBOs, vbos);
 		glDeleteBuffers(maxVAOs, vaos);
 
-		models.Clear();
+		LogNote("...done.");
 	}
 }
