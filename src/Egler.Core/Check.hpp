082a053b9002c75772e121ed9d068e8a82db06af
idafi <none>
2018-07-27 23:37:23 -0500

    add check for >0 gl ids

--- src/Egler.Core/Check.hpp
+++ src/Egler.Core/Check.hpp
@@ -8,6 +8,9 @@
 
 #define Check(cond) _Check((cond), BadArgumentException, "!! Condition check ('" #cond "') failed !!")
 #define CheckPtr(ptr) _Check((ptr) != nullptr, NullPtrException, "!! Pointer '" #ptr "' is null !!")
+#define CheckID(id) _Check((id) > 0, BadArgumentException, "!! ID '" #id "' (%i) is invalid !!", id)
+#define CheckSign(n) _Check((n) >= 0, OutOfRangeException, "!! Value of '" #n "' (%i) is negative !!", (n))
 #define CheckIndex(index, len) _Check(((index) > -1) && ((index) < (len)), OutOfRangeException, \
-    "!! Array index '" #index "' is out of range (between 0 and %i)", (len) - 1)
-#define CheckSign(n) _Check((n) >= 0, OutOfRangeException, "!! Value of '" #n "' is negative (%i)", (n))
+    "!! Array index '" #index "' (%i) is out of range (between 0 and %i) !!", index, (len) - 1)
+#define CheckRange(n, len) _Check(((n) > -1) && ((n) <= (len)), OutOfRangeException, \
+    "!! Number '" #n "' (%i) is out of range (between 0 and %i) !!", n, len)
