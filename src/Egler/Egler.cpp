0d55a67eb212b35f5bc463673727e8018042d893
idafi <none>
2018-07-29 18:52:51 -0500

    implement exe using new entity managers

--- src/Egler/Egler.cpp
+++ src/Egler/Egler.cpp
@@ -3,7 +3,8 @@
 #include "../Egler.Core/Logging/Logging.hpp"
 #include "../Egler.Core/Video/Video.hpp"
 #include "../Egler.FBX/FBXImporter.hpp"
-#include "Entity.hpp"
+
+#include "Egler.hpp"
 
 using namespace Egler::Core;
 using namespace Egler::Logging;
@@ -22,21 +23,14 @@ namespace Egler
     const char * const fragFile = "data/shader.frag";
     const char * const fbxFile = "data/hello.fbx";
 
-    static constexpr int maxEntities = 8;
-
-    static constexpr float cameraFOV = 45;
-    static constexpr float zNear = 1;
-    static constexpr float zFar = 45;
-
     ConsoleLogger consoleLogger;
     FileLogger fileLogger("log.txt");
 
     FBXImporter fbxImporter;
     ModelBuffer modelBuffer;
 
-    GLContext *context;
-    Entity entities[maxEntities];
-    int entityCount;
+    EglerContext *egler;
+    EntityMan::Ptr entity;
 
     void Init()
     {
@@ -56,24 +50,20 @@ namespace Egler
         ShaderSourceFile vert(vertStr.c_str(), ShaderType::Vertex, vertFile);
         ShaderSourceFile frag(fragStr.c_str(), ShaderType::Fragment, fragFile);
         ShaderSourceFile files[] = { vert, frag };
-        ShaderSource shaders[] = { ShaderSource(files, 2) };
-
-        GLContextData data(windowName, windowRect, shaders, 1);
-        context = new GLContext(data);
+        ShaderSource shaderSrc = ShaderSource(files, 2);
 
         ModelData modelData;
         modelData.VertexPositions = modelBuffer.VertexPositions;
         modelData.VertexColors = modelBuffer.VertexColors;
         modelData.Indices = modelBuffer.Indices;
-
         fbxImporter.ImportModel(fbxFile, &modelData);
-        Shader& shader = context->GetShader(0);
 
-        entities[0].Model = context->Models().Allocate(modelData);
-        entities[0].Material = context->Materials().Allocate(shader);
-        entities[0].TRSMatrix = Mat4::Identity();
-        entities[0].SetPosition(Vector3(0, 0, -20));
-        entityCount++;
+        egler = new EglerContext(windowName, windowRect);
+        
+        auto model = egler->Models().Create(*egler, modelData);
+        auto shader = egler->Shaders().Create(*egler, shaderSrc);
+        auto material = egler->Materials().Create(*egler, shader);
+        entity = egler->Entities().Create(*egler, model, material);
     }
 
     bool ShouldQuit()
@@ -83,59 +73,20 @@ namespace Egler
 
     void Frame()
     {
-        GLWindow& window = context->Window();
+        GLWindow& window = egler->Window();
         Vector4 color(0, 0, 0, 1);
         window.Clear(color, 1);
 
-        Vector2 windowSize = window.Size();
-        float aspect = windowSize[0] / windowSize[1];
-        Matrix pspMatrix = Mat4::Perspective(cameraFOV, zNear, zFar, aspect);
+        egler->Entities().Update(*egler);
 
-        const byte * const keys = SDL_GetKeyboardState(nullptr);
-
-        for(int i = 0; i < entityCount; i++)
-        {
-            Entity& ent = entities[i];
-            Model& model = context->Models()[ent.Model];
-            Material& material = context->Materials()[ent.Material];
-
-            Vector3 translate;
-            if(keys[SDL_SCANCODE_RIGHT])
-            { translate[0] += 0.1f; }
-            if(keys[SDL_SCANCODE_LEFT])
-            { translate[0] -= 0.1f; }
-            if(keys[SDL_SCANCODE_UP])
-            { translate[1] += 0.1f; }
-            if(keys[SDL_SCANCODE_DOWN])
-            { translate[1] -= 0.1f; }
-
-            Vector3 scale = ent.GetScale();
-            if(keys[SDL_SCANCODE_D])
-            { scale[0] += 0.1f; }
-            if(keys[SDL_SCANCODE_A])
-            { scale[0] -= 0.1f; }
-            if(keys[SDL_SCANCODE_W])
-            { scale[1] += 0.1f; }
-            if(keys[SDL_SCANCODE_S])
-            { scale[1] -= 0.1f; }
-
-            ent.Translate(translate.ClampMagnitude(0, 1));
-            ent.SetScale(scale);
-
-            material.SetProperty("perspectiveMatrix", pspMatrix);
-            material.SetProperty("localToCameraMatrix", ent.TRSMatrix);
-            context->Window().DrawModel(model, material);
-        }
-
-        context->Window().Present();
-        
+        egler->Window().Present();
         Core::Delay(16);
     }
 
     int Quit(const int code)
     {
-        if(context)
-        { delete context; }
+        if(egler)
+        { delete egler; }
 
         LogNote("Shutting down Core...");
         Core::Quit();
@@ -152,22 +103,22 @@ namespace Egler
 
 int main(int argc, char **argv)
 {
-    //try
-    //{
+    try
+    {
         Egler::Init();
 
         while(!Egler::ShouldQuit())
         { Egler::Frame(); }
 
         return Egler::Quit(0);
-    //}
-    //catch(const Egler::EglerException& e)
-    //
-      //  LogFailure(e.what());
-       // LogFailure("Press Enter to exit...");
-       // getchar();
-       // getchar();
-    //}
+    }
+    catch(const Egler::EglerException& e)
+    {
+        LogFailure(e.what());
+        LogFailure("Press Enter to exit...");
+        getchar();
+        getchar();
+    }
 
-    //return Egler::Quit(1);
+    return Egler::Quit(1);
 }
