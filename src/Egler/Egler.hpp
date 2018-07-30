34d51d8d409948e3bfc4e981532e6cfe5507b668
idafi <none>
2018-07-29 19:21:21 -0500

    forward declare EglerContext

--- src/Egler/Egler.hpp
+++ src/Egler/Egler.hpp
@@ -11,12 +11,13 @@ namespace Egler
 	static constexpr int MaxModels = 64;
 	static constexpr int MaxMaterials = 96;
 	static constexpr int MaxEntities = 16;
-}
 
-#include "Entity.hpp"
+	class EglerContext;
+}
 
 #include "ShaderMan.hpp"
 #include "MaterialMan.hpp"
 #include "ModelMan.hpp"
+#include "Entity.hpp"
 #include "EntityMan.hpp"
 #include "EglerContext.hpp"
